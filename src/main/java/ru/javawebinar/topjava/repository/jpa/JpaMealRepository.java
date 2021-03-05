4b21b054f1f0bdf98ad165909351b70beaaae4b4
tweek <tweeky92@gmail.com>
2021-03-05 18:31:08 +0300

    HW4

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -2,32 +2,51 @@ package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public class JpaMealRepository implements MealRepository {
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        meal.setUser(em.getReference(User.class, userId));
+        if(meal.isNew()){
+            em.persist(meal);
+            return meal;
+        } else if (get(meal.id(), userId) == null){
+            return null;
+        }
+        return em.merge(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return em.createNamedQuery(Meal.DELETE)
+                .setParameter("id", id)
+                .setParameter("userId", userId)
+                .executeUpdate() != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Meal meal = em.find(Meal.class, id);
+        return meal.getUser().getId() == userId ? meal : null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return em.createNamedQuery(Meal.GET_ALL, Meal.class)
+                .setParameter("userId", userId)
+                .getResultList();
     }
 
     @Override
