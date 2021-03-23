98c18498744c7858072a518d00646e070b2a3bf5
tweek <tweeky92@gmail.com>
2021-03-24 00:25:29 +0300

    HW6

--- src/main/java/ru/javawebinar/topjava/web/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/JspMealController.java
@@ -6,31 +6,53 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.meal.AbstractMealController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/meals")
-public class JspMealController {
+public class JspMealController extends AbstractMealController {
 
-    @Autowired
-    MealService mealService;
-
-    @PostMapping
-    public String
-
-    @GetMapping
-    public String getMeals(Model model){
-        model.addAttribute("meals",
-                MealsUtil.getTos(mealService.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay()));
-        return "meals";
+    public JspMealController(MealService service) {
+        super(service);
     }
 
-    @GetMapping
-    public String delete(HttpServletRequest request){
+    @Override
+    @GetMapping("")
+    public Meal get(int id) {
+        return super.get(id);
+    }
 
-        return "meals";
+    @Override
+    public void delete(int id) {
+        super.delete(id);
+    }
+
+    @Override
+    public List<MealTo> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    public Meal create(Meal meal) {
+        return super.create(meal);
+    }
+
+    @Override
+    public void update(Meal meal, int id) {
+        super.update(meal, id);
+    }
+
+    @Override
+    public List<MealTo> getBetween(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
+        return super.getBetween(startDate, startTime, endDate, endTime);
     }
 }
