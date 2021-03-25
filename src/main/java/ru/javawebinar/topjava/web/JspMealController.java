21ed290ba0d00d67399ff57f10ece0359cf87ed2
tweek <tweeky92@gmail.com>
2021-03-25 19:19:53 +0300

    HW6

--- src/main/java/ru/javawebinar/topjava/web/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/JspMealController.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +14,15 @@ import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.meal.AbstractMealController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
 @Controller
 @RequestMapping(value = "/meals")
@@ -25,34 +32,52 @@ public class JspMealController extends AbstractMealController {
         super(service);
     }
 
-    @Override
-    @GetMapping("")
-    public Meal get(int id) {
-        return super.get(id);
+    @GetMapping
+    public String doGet(HttpServletRequest request) {
+        String action = request.getParameter("action");
+
+        switch (action == null ? "all" : action) {
+            case "delete" -> {
+                super.delete(Integer.parseInt(request.getParameter("id")));
+                request.setAttribute("meals", super.getAll());
+                return "meals";
+            }
+            case "create", "update" -> {
+                final Meal meal = "create".equals(action) ?
+                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
+                        super.get(Integer.parseInt(request.getParameter("id")));
+                request.setAttribute("meal", meal);
+                return "mealForm";
+            }
+            case "filter" -> {
+                LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+                LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+                LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+                LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+                request.setAttribute("meals", super.getBetween(startDate, startTime, endDate, endTime));
+                return "meals";
+            }
+            default -> {
+                request.setAttribute("meals", super.getAll());
+                return "meals";
+            }
+        }
     }
 
-    @Override
-    public void delete(int id) {
-        super.delete(id);
-    }
+    @PostMapping
+    public String doPost(HttpServletRequest request) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
 
-    @Override
-    public List<MealTo> getAll() {
-        return super.getAll();
-    }
-
-    @Override
-    public Meal create(Meal meal) {
-        return super.create(meal);
-    }
-
-    @Override
-    public void update(Meal meal, int id) {
-        super.update(meal, id);
-    }
-
-    @Override
-    public List<MealTo> getBetween(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
-        return super.getBetween(startDate, startTime, endDate, endTime);
+        if (StringUtils.hasLength(request.getParameter("id"))) {
+            super.update(meal, Integer.parseInt(request.getParameter("id")));
+        } else {
+            super.create(meal);
+        }
+        request.setAttribute("meals", super.getAll());
+        return "meals";
     }
 }
